e0dad538e4259520098a6bd6fcdd4a469c965661
Chris Ovalle <chrisovalle1@gmail.com>
2020-11-12 11:47:15 -0500

    launch app success

--- MyFood/lib/main.dart
+++ MyFood/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:MyFoodLogin/theme/approutes.dart';
 import 'package:MyFoodLogin/view/MainPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-import './view/Login.dart';
+import 'package:MyFoodLogin/view/Login.dart';
 import 'package:flutter/material.dart';
 // import 'package:firebase_core/firebase_core.dart';
 
@@ -17,7 +17,7 @@ class MyFood extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginScreen(),
+      home: MainPage(),
       routes: Routes.define(),
     );
   }
