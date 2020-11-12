b15b9b374932f91ba822f68c3a5ed39eec1e4cb1
Chris Ovalle <chrisovalle1@gmail.com>
2020-11-12 11:48:01 -0500

    login screen change

--- MyFood/lib/main.dart
+++ MyFood/lib/main.dart
@@ -17,7 +17,7 @@ class MyFood extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
+      home: LoginScreen(),
       routes: Routes.define(),
     );
   }
